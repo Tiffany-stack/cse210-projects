c1daa2d1a785f91f7cff7df75c09d001e74f58d7
Tiffany-stack <124050667+Tiffany-stack@users.noreply.github.com>
2023-11-03 23:14:57 +0800

    Update ReflectionActivity.cs

--- prove/Develop04/ReflectionActivity.cs
+++ prove/Develop04/ReflectionActivity.cs
@@ -6,44 +6,26 @@ class ReflectionActivity
     public static void StartReflectionActivity()
     {
         Console.WriteLine("Welcome to the Reflection Activity.");
-        Console.WriteLine("This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.");
+        Console.WriteLine("Take a moment to reflect on your experiences.");
 
         // Allow the user to set the duration for reflection (in seconds)
         Console.Write("Enter the duration (in seconds): ");
         if (int.TryParse(Console.ReadLine(), out int duration))
         {
-            Console.WriteLine("Starting the Reflection Activity. Get ready to reflect.");
+            Console.WriteLine("Starting the Reflection Activity. Begin your reflection.");
 
             // Reflection guidance
             for (int seconds = 0; seconds < duration; seconds++)
             {
-                // Display random prompts for reflection
-                DisplayRandomPrompt();
-
-                // Pause for a moment
-                Thread.Sleep(1000);
+                Console.WriteLine("Keep reflecting...");
+                Thread.Sleep(1000); // Pause for 1 second
             }
 
-            Console.WriteLine("You've completed the Reflection Activity. Take time to acknowledge your strengths and resilience.");
+            Console.WriteLine("You've completed the Reflection Activity. Continue to reflect or move on.");
         }
         else
         {
             Console.WriteLine("Invalid duration. Please enter a valid number of seconds.");
         }
-    }
-
-    // Display a random reflection prompt
-    private static void DisplayRandomPrompt()
-    {
-        string[] prompts = {
-            "Think of a time when you stood up for someone else.",
-            "Think of a time when you did something really difficult.",
-            "Think of a time when you helped someone in need.",
-            "Think of a time when you did something truly selfless."
-        };
-
-        Random random = new Random();
-        int index = random.Next(prompts.Length);
-        Console.WriteLine(prompts[index]);
     }
 }
