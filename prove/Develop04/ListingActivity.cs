eb0e8fa68538ef13cc9aeebad091815f4a13f4c6
Tiffany-stack <124050667+Tiffany-stack@users.noreply.github.com>
2023-11-03 23:13:11 +0800

    Update ListingActivity.cs

--- prove/Develop04/ListingActivity.cs
+++ prove/Develop04/ListingActivity.cs
@@ -6,45 +6,26 @@ class ListingActivity
     public static void StartListingActivity()
     {
         Console.WriteLine("Welcome to the Listing Activity.");
-        Console.WriteLine("This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.");
+        Console.WriteLine("Think about positive aspects of your life and list them.");
 
         // Allow the user to set the duration for listing (in seconds)
         Console.Write("Enter the duration (in seconds): ");
         if (int.TryParse(Console.ReadLine(), out int duration))
         {
-            Console.WriteLine("Starting the Listing Activity. Get ready to list.");
+            Console.WriteLine("Starting the Listing Activity. Begin listing positive things.");
 
             // Listing guidance
             for (int seconds = 0; seconds < duration; seconds++)
             {
-                // Display a random listing prompt
-                DisplayRandomPrompt();
-
-                // Pause for a moment
-                Thread.Sleep(1000);
+                Console.WriteLine("Keep listing...");
+                Thread.Sleep(1000); // Pause for 1 second
             }
 
-            Console.WriteLine("You've completed the Listing Activity. Count the blessings in your life.");
+            Console.WriteLine("You've completed the Listing Activity. Celebrate the positivity in your life.");
         }
         else
         {
             Console.WriteLine("Invalid duration. Please enter a valid number of seconds.");
         }
-    }
-
-    // Display a random listing prompt
-    private static void DisplayRandomPrompt()
-    {
-        string[] prompts = {
-            "Who are people that you appreciate?",
-            "What are personal strengths of yours?",
-            "Who are people that you have helped this week?",
-            "When have you felt the Holy Ghost this month?",
-            "Who are some of your personal heroes?"
-        };
-
-        Random random = new Random();
-        int index = random.Next(prompts.Length);
-        Console.WriteLine(prompts[index]);
     }
 }
